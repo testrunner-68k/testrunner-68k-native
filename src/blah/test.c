6e29ef4019e45548c0ccb0c9f36ac5f3b1330d5d
Mikael Kalms <mikael@kalms.org>
2022-02-27 14:44:03 +0100

    Verify that executable begins with HUNK_HEADER

--- src/blah/test.c
+++ src/blah/test.c
@@ -1,24 +1,46 @@
+#include "doshunks.h"
+#include "log.h"
 #include "LRUCachedFile.h"
+#include "LRUCachedFileReader.h"
 #include <stdio.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+bool readThingy(const char* fileName)
+{
+    LRUCachedFile lruCachedFile;
+    if (!LRUCachedFile_open(&lruCachedFile, fileName)) {
+        return false;
+    }
+
+    LRUCachedFileReader lruCachedFileReader;
+    LRUCachedFileReader_init(&lruCachedFile, &lruCachedFileReader);
+
+    uint32_t value;
+    if (!LRUCachedFileReader_readU32BigEndian(&lruCachedFileReader, &value) || value != HUNK_HEADER) {
+        log_error("File must begin with HUNK_HEADER");
+        LRUCachedFile_close(&lruCachedFile);
+        return false;
+    }
+
+    log_info("yep!");
+
+    LRUCachedFile_close(&lruCachedFile);
+
+    return true;
+}
+
 int main(int argc, const char** argv)
 {
-	// AHPInfo* info;
 
-    // if (argc < 2)
-    // {
-    //     printf("Usage: %s <amiga executable>\n\n", argv[0]);
-    //     return 0;
-    // }
+    if (argc < 2)
+    {
+        printf("Usage: %s <amiga executable>\n\n", argv[0]);
+        return 0;
+    }
 
-    // if (!(info = ahp_parse_file(argv[1])))
-    // 	return 0;
-
-    // ahp_print_info(info, 1);
-
-    // ahp_free(info);
+    if (!readThingy(argv[1]))
+        return 1;
 
     return 0;
 }
